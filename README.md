27800e402413aa8217d9a38450b5cff23638114c
prampey <prudhvi.rampey@gmail.com>
2023-04-26 16:44:42 -0500

    Add support 'functions' endpoint

--- README.md
+++ README.md
@@ -6,4 +6,7 @@ curl "http://localhost:5000/call_contract?contract_address=0xdac17f958d2ee523a22
 
 # Proxy contract
 curl "http://localhost:8000/call_contract?contract_address=0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48&function_name=balanceOf&arguments=['0x742d35Cc6634C0532925a3b844Bc454e4438f44e']" -g
+
+# Get functions of contract (proxy included)
+curl "http://localhost:8000/functions?contract_address=0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48" -g
 ```
--- call_contract.py
+++ call_contract.py
@@ -5,7 +5,7 @@ from functools import cache
 import json
 import ast
 
-ETHERSCAN_API_KEY = '<Your etyhercan api key>'
+ETHERSCAN_API_KEY = 'Your key here'
 
 app = Flask(__name__)
 w3 = Web3(Web3.HTTPProvider('https://eth-mainnet.g.alchemy.com/v2/GLfjhndE2L3YJmb0-hDX635D-ioTIOD6'))
@@ -29,13 +29,23 @@ def get_implementation_address(address):
     impl_addr = w3.to_hex(
         w3.eth.get_storage_at(
             address,
-            "0x7050c9e0f4ca769c69bd3a8ef740bc37934f8e2c036e5a723fd8ee048ed3f8c3", # Storage slot address
+            "0x7050c9e0f4ca769c69bd3a8ef740bc37934f8e2c036e5a723fd8ee048ed3f8c3", # Storage slot address for OZ-based proxies
         )
     )
     impl_addr = '0x' + impl_addr[26:] # Extract last 20 bytes
     if(w3.to_int(hexstr=impl_addr) == 0): # Nothing stored here
         return None
     return w3.to_checksum_address(impl_addr)
+
+# Check if contract is a proxy and replace it with the implementation contract
+def check_and_replace_proxy(contract):
+    proxy_address = contract.address
+    impl_address = get_implementation_address(proxy_address)
+    if impl_address:
+        print('Proxy contract found. Fetching implementation contract ABI...')
+        contract_abi = get_abi(impl_address)
+        contract = w3.eth.contract(address=proxy_address, abi=contract_abi)
+    return contract
 
 @app.route('/call_contract', methods=['GET'])
 def get_contract_data():
@@ -61,11 +71,7 @@ def get_contract_data():
     contract = w3.eth.contract(address=contract_address, abi=contract_abi)
 
     # Check if this is a proxy contract
-    implementation_address = get_implementation_address(contract_address)
-    if implementation_address:
-        print('Proxy contract found. Fetching implementation contract...')
-        contract_abi = get_abi(implementation_address)
-        contract = w3.eth.contract(address=contract_address, abi=contract_abi)
+    contract = check_and_replace_proxy(contract)
 
     try:
         function = contract.functions.__getattribute__(function_name)
@@ -82,6 +88,30 @@ def get_contract_data():
         result = w3.to_int(result)
 
     return jsonify({'result': result}), 200
+
+@app.route('/functions', methods=['GET'])
+def get_contract_functions():
+    contract_address = request.args.get('contract_address')
+
+    if not contract_address:
+        return jsonify({'error': 'Missing required parameter'}), 400
+
+    try:
+        contract_address = w3.to_checksum_address(contract_address)
+    except ValueError:
+        return jsonify({'error': 'Invalid input format'}), 400
+
+    contract_abi = get_abi(contract_address)
+
+    if contract_abi is None:
+        return jsonify({'error': 'Unable to fetch ABI for the contract address'}), 400
+
+    contract = w3.eth.contract(address=contract_address, abi=contract_abi)
+    contract = check_and_replace_proxy(contract)
+
+    functions = [func.fn_name for func in contract.all_functions()]
+
+    return jsonify({'functions': functions}), 200
 
 
 if __name__ == '__main__':
